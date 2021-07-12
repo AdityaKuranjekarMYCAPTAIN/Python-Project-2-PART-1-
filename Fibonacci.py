ee4e596f2d667aaeda616a9bd4802e29a50ba820
Aditya Kuranjekar <81797057+AdityaKuranjekarMYCAPTAIN@users.noreply.github.com>
2021-07-12 14:02:56 +0530

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+n = int(input("How many numbers of fibonacci series do you want to see?: "))
+x = 0
+y = 1
+z = 0
+i = 1
+while (n >= i):
+	print (z)
+	x = y
+	y = z
+	z = x+y
+	i += 1
+#>>>>>>>>>>>>>>>>>>.................HAPPY CODING....................<<<<<<<<<<<<<<<<<<<<<<<  
